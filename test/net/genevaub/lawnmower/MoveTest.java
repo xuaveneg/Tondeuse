86ab1aecdafacf35de0367e066b3df1833e3d744
Xuaveneg <genevaub@xinra.net>
2020-01-26 20:42:18 +0100

    Test sur les mouvements

---
+++ test/net/genevaub/lawnmower/MoveTest.java
@@ -0,0 +1,54 @@
+package net.genevaub.lawnmower;
+
+import org.junit.Test;
+
+import java.util.Optional;
+
+import static org.junit.Assert.assertEquals;
+
+public class MoveTest {
+
+    @Test
+    public void should_find_move_d() {
+        // GIVEN
+
+        // WHEN
+        Optional<Move> d = Move.of('D');
+
+        // THEN
+        assertEquals(Optional.of(Move.RIGHT), d);
+    }
+
+    @Test
+    public void should_find_move_g() {
+        // GIVEN
+
+        // WHEN
+        Optional<Move> g = Move.of('G');
+
+        // THEN
+        assertEquals(Optional.of(Move.LEFT), g);
+    }
+
+    @Test
+    public void should_find_move_a() {
+        // GIVEN
+
+        // WHEN
+        Optional<Move> a = Move.of('A');
+
+        // THEN
+        assertEquals(Optional.of(Move.FORWARD), a);
+    }
+
+    @Test
+    public void should_not_find_unknown_move() {
+        // GIVEN
+
+        // WHEN
+        Optional<Move> k = Move.of('K');
+
+        // THEN
+        assertEquals(Optional.empty(), k);
+    }
+}
