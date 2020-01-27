5fa53a4a8d5928bac286d4e1cd3977b8ab1109bd
Xuaveneg <genevaub@xinra.net>
2020-01-27 01:19:34 +0100

    Nouveau test avec un nouveau fichier d'entrée

--- test/net/genevaub/MainTest.java
+++ test/net/genevaub/MainTest.java
@@ -31,4 +31,26 @@ public class MainTest {
         assertEquals("1 3 N\n" +
                 "5 1 E", output);
     }
+
+    @Test
+    public void should_handle_main_test_from_custom_file() throws ParserException, LawnException {
+        // GIVEN
+        final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        final PrintStream printStream = new PrintStream(outputStream);
+        final LawnBuilder lawnBuilder = new LawnBuilderImpl(printStream);
+        final LineReader lineReader = new LineReaderImpl(lawnBuilder);
+        final Parser fileParser = new ParserImpl(lineReader);
+
+        // WHEN
+        fileParser.parse(FileUtils.getTestResourceFile("fullInput.txt"));
+
+        // THEN
+        final String output = new String(outputStream.toByteArray());
+        assertEquals("5 0 S\n" +
+                "5 7 N\n" +
+                "0 10 W\n" +
+                "3 2 S\n" +
+                "2 9 W\n" +
+                "5 10 W", output);
+    }
 }
